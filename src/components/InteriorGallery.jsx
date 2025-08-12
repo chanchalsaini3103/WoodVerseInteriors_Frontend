cf33aba6d922f3ad3e1efe8d02b0e5ec2d617bb9
chanchal3103 <chanchalsaini3103@gmail.com>
2025-08-12 13:18:15 +0530

    first commit

--- src/components/InteriorGallery.jsx
+++ src/components/InteriorGallery.jsx
@@ -1,32 +1,38 @@
 import React from "react";
 import "../styles/InteriorGallery.css";
 
+// Import images
+import img1 from "../images/1.jpg";
+import img2 from "../images/2.jpg";
+import img3 from "../images/3.jpg";
+import img4 from "../images/4.jpg";
+import img5 from "../images/5.jpg";
+import img6 from "../images/6.jpg";
+
 const InteriorGallery = () => {
   return (
-
     <>
-    <div className="gallery-header">
+      <div className="gallery-header">
         <h2 className="gallery-title">Discover Beautiful Interior Inspirations</h2>
         <button className="view-all-btn">View All</button>
       </div>
-      <div className="gallery-container">
-        
-      {/* First Row: 3:1:1 */}
-      <div className="gallery-row">
-        <img src="/src/images/1.jpg" alt="Landscape" className="landscape" />
-        <img src="/src/images/2.jpg" alt="Portrait" className="portrait" />
-        <img src="/src/images/3.jpg" alt="Portrait" className="portrait" />
-      </div>
 
-      {/* Second Row: 1:3:1 */}
-      <div className="gallery-row">
-        <img src="/src/images/4.jpg" alt="Portrait" className="portrait" />
-        <img src="/src/images/5.jpg" alt="Landscape" className="landscape" />
-        <img src="/src/images/6.jpg" alt="Portrait" className="portrait" />
+      <div className="gallery-container">
+        {/* First Row: 3:1:1 */}
+        <div className="gallery-row">
+          <img src={img1} alt="Landscape" className="landscape" />
+          <img src={img2} alt="Portrait" className="portrait" />
+          <img src={img3} alt="Portrait" className="portrait" />
+        </div>
+
+        {/* Second Row: 1:3:1 */}
+        <div className="gallery-row">
+          <img src={img4} alt="Portrait" className="portrait" />
+          <img src={img5} alt="Landscape" className="landscape" />
+          <img src={img6} alt="Portrait" className="portrait" />
+        </div>
       </div>
-    </div>
     </>
-  
   );
 };
 
